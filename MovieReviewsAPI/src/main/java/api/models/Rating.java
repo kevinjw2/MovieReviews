3b76cb1caa62d5922a729bfafc41136c6176d9c2
Kevin White <kjw72698@gmail.com>
2021-01-11 11:03:23 -0600

    added model classes

--- MovieReviewsAPI/src/main/java/api/models/Rating.java
+++ MovieReviewsAPI/src/main/java/api/models/Rating.java
@@ -2,5 +2,31 @@ package api.models;
 
 public class Rating {
 
+    private int personalRating;
+    private String personalComments;
+    private int movieId;
 
+    public int getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(int personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    public String getPersonalComments() {
+        return personalComments;
+    }
+
+    public void setPersonalComments(String personalComments) {
+        this.personalComments = personalComments;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
 }
