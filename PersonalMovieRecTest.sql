46bcd1635f54da6dfe8f7d3dc8dd4acfaa7214c6
Michael Burns <75638036+mikeburns22@users.noreply.github.com>
2021-01-10 21:43:30 -0500

    Add files via upload

---
+++ PersonalMovieRecTest.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS personalMovieRecsTest;
+
+CREATE DATABASE personalMovieRecsTest;
+
+USE personalMovieRecsTest;
+
+CREATE TABLE Movie (
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(50) NOT NULL,
+    Director VARCHAR(30) NOT NULL,
+    Duration INT,
+    ReleaseYear CHAR(4) NOT NULL,
+    LeadActor VARCHAR(30) NOT NULL,
+    LeadActress VARCHAR(30) NOT NULL,
+    Genre VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Rating (
+	PersonalRating INT,
+    MovieId INT,
+    FOREIGN KEY fk_Movie_Rating (MovieId)
+		REFERENCES Movie(Id)
+);
+
+CREATE TABLE Comments (
+	Description VARCHAR(500) NULL,
+    MovieId int,
+    FOREIGN KEY fk_Movie_Comments (MovieId)
+		REFERENCES Movie(Id)
+);
